d902355f10b3524c19bfab62daee6c8516b63775
RINGO88-lab <2276526438@qq.com>
2026-07-22 21:31:56 +0800

    Day 8: 面向对象重构 - 方法接受参数，main.py负责输入输出

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,31 @@
-﻿from models import UserManager, main
+from models import UserManager
+
+def main():
+    um = UserManager()
+    while True:
+        c = input("1登录 2退出 3注册 4改密 5删除 6查看：")
+        if c == "1":
+            a = input("账号：")
+            p = input("密码：")
+            print(um.login(a, p))
+        elif c == "2":
+            break
+        elif c == "3":
+            a = input("账号：")
+            p = input("密码：")
+            print(um.register(a, p))
+        elif c == "4":
+            a = input("账号：")
+            o = input("旧密码：")
+            n = input("新密码：")
+            print(um.change_password(a, o, n))
+        elif c == "5":
+            a = input("账号：")
+            c2 = input("确认？(y): ")
+            if c2 == "y":
+                print(um.delete(a))
+        elif c == "6":
+            print(um.list_users())
 
 if __name__ == "__main__":
     main()
--- backend/models.py
+++ backend/models.py
@@ -2,64 +2,32 @@ class UserManager:
     def __init__(self):
         self.users = {}
 
-    def user_reg(self):
-        while True:
-            a = input("请输入账号：")
-            p = input("请输入密码：")
-            if a in self.users:
-                print("已存在")
-                continue
-            self.users[a] = p
-            print("注册成功")
-            break
+    def register(self, account, password):
+        if account in self.users:
+            return "已存在"
+        self.users[account] = password
+        return "注册成功"
 
-    def login(self):
-        for i in range(3):
-            a = input("请输入账号：")
-            p = input("请输入密码：")
-            if a in self.users and self.users[a] == p:
-                return "登录成功"
-        return "已禁止登录"
+    def login(self, account, password):
+        if account in self.users and self.users[account] == password:
+            return "登录成功"
+        return "账号或密码错误"
 
-    def change_password(self):
-        while True:
-            a = input("请输入账号：")
-            if a not in self.users:
-                print("不存在")
-                continue
-            p = input("请输入旧密码：")
-            if p == self.users[a]:
-                self.users[a] = input("请输入新密码：")
-                print("修改成功")
-                break
+    def change_password(self, account, old_pw, new_pw):
+        if account not in self.users:
+            return "账号不存在"
+        if self.users[account] != old_pw:
+            return "旧密码错误"
+        self.users[account] = new_pw
+        return "修改成功"
 
-    def delete(self):
-        a = input("请输入账号：")
-        if a not in self.users:
-            print("不存在")
-            return
-        c = input("确认删除？(y/n)：")
-        if c == "y":
-            del self.users[a]
-            print("删除成功")
+    def delete(self, account):
+        if account not in self.users:
+            return "账号不存在"
+        del self.users[account]
+        return "删除成功"
 
     def list_users(self):
         if not self.users:
-            print("暂无用户")
-            return
-        for u in self.users:
-            print(u)
-
-def main():
-    um = UserManager()
-    while True:
-        c = input("1登录 2退出 3注册 4改密 5删除 6查看：")
-        if c == "1": print(um.login())
-        elif c == "2": break
-        elif c == "3": um.user_reg()
-        elif c == "4": um.change_password()
-        elif c == "5": um.delete()
-        elif c == "6": um.list_users()
-
-if __name__ == "__main__":
-    main()
+            return "暂无用户"
+        return "\n".join(self.users.keys())
